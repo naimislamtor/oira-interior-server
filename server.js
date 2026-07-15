c2198c061e5154927d7112cfad9fe30cc1fde0a9
amirhamja55880 <amirhamza125986@gmail.com>
2026-07-15 13:00:21 +0600

    adedting a frontend url

--- server.js
+++ server.js
@@ -9,7 +9,25 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://www.oriainterior.com",
+  "https://oriainterior.com",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
